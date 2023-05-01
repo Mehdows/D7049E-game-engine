8a5b0674493cb6b12f175375dadccba1500edd1e
Alexander Eklund <alexeklund460@gmail.com>
2023-05-01 19:57:17 +0200

    Remove the 2d player to simplify

--- ArenaGame/Ecs/Core/Archetype/EArchetype.cs
+++ ArenaGame/Ecs/Core/Archetype/EArchetype.cs
@@ -3,11 +3,10 @@
 public enum EArchetype {
     StaticMesh = 0, // PostionComponent, RotationComponent, MeshComponent
     DynamicMesh = 1, // StaticMesh, VelocityComponent 
-    Player = 2, // DynamicMesh ,  AnimationComponent, InputComponent
     Enemy = 3, // DynamicMesh,  AnimationComponent, AIComponent
     Projectile = 4, // DynamicMesh 
     Light = 5,  // PositionComponent, LightComponent, RotationComponent
     Particle = 6, //
     Trigger = 7,
-    Player3D = 8 // TransformComponent, InputComponent
+    Player = 8 // TransformComponent, InputComponent
 } 
