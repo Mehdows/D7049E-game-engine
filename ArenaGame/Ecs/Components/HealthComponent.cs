b63697410a1ab47ee640f076d5139fc82c705fd3
Mehdows <marcus99.ma@gmail.com>
2023-05-02 15:49:22 +0200

    added untested health component and system

---
+++ ArenaGame/Ecs/Components/HealthComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ArenaGame.Ecs.Components
+{
+    internal class HealthComponent : IComponent
+    {
+        public float Health { get; set; }
+        public float CurrentHealth { get; set; }
+        public float Regeneration { get; set; }
+        
+        public HealthComponent(float health, float currentHealth, float regeneration)
+        {
+            Health = health;
+            CurrentHealth = currentHealth;
+            Regeneration = regeneration;
+        }
+        
+        public HealthComponent() 
+        {
+            Health = 100;
+            CurrentHealth = 100;
+            Regeneration = 0;
+        }
+    }
+}
---
+++ ArenaGame/Ecs/Systems/HealthSystem.cs
@@ -0,0 +1,51 @@
+﻿using ArenaGame.Ecs.Components;
+using Microsoft.Xna.Framework;
+
+
+namespace ArenaGame.Ecs.Systems
+{
+    public class HealthSystem: ISystem
+    {
+        private readonly Entity HealthComponent;
+        private float damageTaken = 0f;
+
+        public HealthSystem(Entity healthComponenet) 
+        { 
+            HealthComponent = healthComponenet;
+        }
+        public void Update(GameTime gameTime)
+        {
+            float elapsedTime = gameTime.ElapsedGameTime.Seconds;
+
+            float health = ((HealthComponent)HealthComponent.GetComponent<HealthComponent>()).Health;
+            float currentHealth = ((HealthComponent)HealthComponent.GetComponent<HealthComponent>()).CurrentHealth;
+            float regeneration = ((HealthComponent)HealthComponent.GetComponent<HealthComponent>()).Regeneration * elapsedTime;
+            if (health < currentHealth+regeneration) { 
+                currentHealth = health;
+            }
+            else
+            {
+                currentHealth += regeneration;
+            }
+
+            if (currentHealth > damageTaken)
+            {
+                currentHealth -= damageTaken;
+                
+            }
+            else
+            {
+                currentHealth = 0;
+            }
+
+            damageTaken = 0;
+
+            ((HealthComponent)HealthComponent.GetComponent<HealthComponent>()).CurrentHealth = currentHealth;
+        }
+
+        public void DealDamage(float  damage) 
+        {
+            damageTaken += damage;
+        }
+    }
+}
