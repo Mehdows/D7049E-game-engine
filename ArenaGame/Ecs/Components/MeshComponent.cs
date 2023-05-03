70f089a29f3679a1e2ba52b5a34ff04cb4774c0d
Alexander Eklund <alexeklund460@gmail.com>
2023-05-03 16:55:54 +0200

    Removed the rotation of the collision capsule

--- ArenaGame/Ecs/Components/MeshComponent.cs
+++ ArenaGame/Ecs/Components/MeshComponent.cs
@@ -22,9 +22,12 @@ public class MeshComponent : IComponent
         this.modelPath = modelPath;
     }
 
+    // TODO Ändra så att Capsule inte är hårdkodad
     public new void LoadContent(ContentManager contentManager)
     {
-        Capsule = new Capsule(new Vector3(0, 40, 0), 20, 5);
+        Capsule = new Capsule(new Vector3(0, 20, 0), 20, 5, 1);
+        Capsule.AngularDamping = 0f;
+        Capsule.LocalInertiaTensorInverse = new Matrix3x3(0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f);
         Transform =  Matrix.CreateScale(Capsule.Radius/30, Capsule.Length/110, Capsule.Radius/30);
         Model = contentManager.Load<Model>(modelPath);
         
